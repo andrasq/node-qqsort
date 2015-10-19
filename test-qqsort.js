87e704fd59d2baa9557b2083220e88ed5bd7de21
Andras <andras@andrasq.com>
2015-10-18 23:52:27 -0400

    more tests; 1.0.1

--- test-qqsort.js
+++ test-qqsort.js
@@ -17,6 +17,7 @@ module.exports = {
     'should sort array 1': function(t) {
         var a1 = [1]
         qqsort(a1, function(err) {
+            t.ifError(err)
             t.deepEqual(a1, [1])
             t.done()
         })
@@ -25,6 +26,7 @@ module.exports = {
     'should return the array': function(t) {
         var a1 = [1]
         qqsort(a1, function(err, a2) {
+            t.ifError(err)
             t.equal(a1, a2)
             t.done()
         })
@@ -33,6 +35,7 @@ module.exports = {
     'should sort 2 identical': function(t) {
         var a = [1,1]
         qqsort(a, function(err) {
+            t.ifError(err)
             t.deepEqual(a, [1,1])
             t.done()
         })
@@ -41,6 +44,7 @@ module.exports = {
     'should sort 3 identical': function(t) {
         var a = [1,1,1]
         qqsort(a, function(err) {
+            t.ifError(err)
             t.deepEqual(a, [1,1,1])
             t.done()
         })
@@ -49,6 +53,7 @@ module.exports = {
     'should sort array 2 sorted': function(t) {
         var a1 = [1, 2]
         qqsort(a1, function(err) {
+            t.ifError(err)
             t.deepEqual(a1, [1, 2])
             t.done()
         })
@@ -57,6 +62,7 @@ module.exports = {
     'should sort array 3 ordered': function(t) {
         var a1 = [1, 2, 3]
         qqsort(a1, function(err) {
+            t.ifError(err)
             t.deepEqual(a1, [1,2,3])
             t.done()
         })
@@ -65,6 +71,7 @@ module.exports = {
     'should sort array 2': function(t) {
         var a1 = [2, 1]
         qqsort(a1, function(err) {
+            t.ifError(err)
             t.deepEqual(a1, [1, 2])
             t.done()
         })
@@ -73,6 +80,7 @@ module.exports = {
     'should sort array 3': function(t) {
         var a1 = [2, 1, 3]
         qqsort(a1, function(err) {
+            t.ifError(err)
             t.deepEqual(a1, [1,2,3])
             t.done()
         })
@@ -81,7 +89,42 @@ module.exports = {
     'should use the passed-in comparator': function(t) {
         var a1 = [1, 2, 3]
         qqsort(a1, function(a,b) { return (a < b) ? 1 : (a > b) ? -1 : 0 }, function(err) {
+            t.ifError(err)
             t.deepEqual(a1, [3,2,1])
+            t.done()
+        })
+    },
+
+    'should sort 10 items': function(t) {
+        var a1 = [ 10, 9, 8, 8, 6, 5, 4, 3, 2, 2 ]
+        qqsort(a1, function(err) {
+            t.ifError(err)
+            t.deepEqual(a1, [2,2,3,4,5,6,8,8,9,10])
+            t.done()
+        })
+    },
+
+    'should not block event loop': function(t) {
+        var a1 = []
+        //for (var i=0; i<100000; i++) a1.push(100000-i)
+        //for (var i=0; i<100000; i++) a1.push((Math.random() * 10000) >>> 0)
+        //var compar = null
+        for (var i=0; i<100000; i++) { var v = {} ; v['a'] = Math.random() * 10000 >>> 0 ; a1.push(v) }
+        var compar = function(a,b) { return (a['a'] < b['a']) ? -1 : (a['a'] > b['a']) ? 1 : 0 }
+        var lastTick = Date.now(), maxTick = 0
+        var heartbeatDone = false
+        setTimeout(function heartbeatTimer() {
+            var now = Date.now()
+            if (now - lastTick > maxTick) maxTick = now - lastTick
+            lastTick = now
+            if (!heartbeatDone) setTimeout(heartbeatTimer, 2)
+        }, 2)
+        qqsort(a1, compar, function(err) {
+            t.ifError()
+            heartbeatDone = true
+            // blocked: 100k 5ms, 200k 10ms, 500k 25ms, 1m 60-120ms
+            t.ok(maxTick < 20, "event loop blocked " + maxTick + " ms")
+            for (var i=1; i<a1.length; i++) t.ok(a1[i-1] <= a1[i], "" + a1[i-1] + " v. " + a1[i])
             t.done()
         })
     },
@@ -94,6 +137,7 @@ module.exports = {
             a2 = a1.slice(0).sort(function(a,b) { return (a < b) ? -1 : (a > b) ? 1 : 0 })
             var t1 = Date.now()
             qqsort(a1, function(err) {
+                t.ifError(err)
                 t.deepEqual(a1, a2)
                 //console.log("AR: sorted %d in %d ms", nitems, Date.now() - t1)
                 // .32 sec to sort 100k numbers, .34 for 100k strings
